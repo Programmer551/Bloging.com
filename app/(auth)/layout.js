2bf7392778d9546221836747db34630301831fed
Duraid <m.duraid551@gmail.com>
2024-02-03 09:11:21 +0500

    Update

--- app/(auth)/layout.js
+++
@@ -1,6 +0,0 @@
-const Layout = ({ children }) => {
-  return (
-    <div className='flex justify-center items-center h-screen'>{children}</div>
-  );
-};
-export default Layout;
--- components/shared/blog-create.jsx
+++
@@ -1,8 +0,0 @@
-// import { submitBlog } from "../../utils/actions";
-// const Blog_Create = ({ id }) => {
-//   return (
-   
-//   );
-// };
-
-// export default Blog_Create;
